364fdbb4f57f5221c211fb534f1f52feb7a51602
danny galimi <dannygalimi13@aim.com>
2021-04-12 15:22:20 -0400

    added session view folder, contoller and route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :guitars, through: :reviews
+    has_many :reviewed_guitars, through: :reviews, source: :guitar
     has_many :guitars
 end
