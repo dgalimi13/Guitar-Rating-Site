3cd98a715558f4d8932b1e078991f93918227829
danny galimi <dannygalimi13@aim.com>
2021-04-13 14:33:53 -0400

    brushed up reviews new forum and reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,16 @@
 class ReviewsController < ApplicationController
 
 def new
-  @guitar = Guitar.find_by_id(params[:id])
-  @review = @guitar.build_review
+  @guitar = Guitar.find_by_id(params[:guitar_id])
+  @review = @guitar.reviews.build
 end 
 
 def index
 
 end 
+
+def review_params
+    params.require(:review).permit(:guitar_id, :content, :stars, :title)
+end
 
 end
--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -3,7 +3,10 @@ class Guitar < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
-accepts_nested_attributes_for :make
+  accepts_nested_attributes_for :make
 
-
+    def make_attributes(attributes)
+      make = Make.find_or_create_by(attributes) if !name.empty
+    end
 end
+ 
