0634c037d45a1c58c0e96b08646a1e5df5fe9a67
danny galimi <dannygalimi13@aim.com>
2021-04-13 14:14:06 -0400

    added to reviews new fourm

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+
+def new
+  @guitar = Guitar.find_by_id(params[:id])
+  @review = @guitar.build_review
+end 
+
+def index
+
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
  
 
   resources :reviews
-  resources :guitars
+  resources :guitars do 
+    resources :reviews, only: [:new, :index]
+  end 
   resources :makes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
