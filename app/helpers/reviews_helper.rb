ae264c23154086b97d17f84959f2888014698e1a
danny galimi <dannygalimi13@aim.com>
2021-04-15 14:30:44 -0400

    added omniauth gem and got google clientID/secret

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
-end
+
+    def display_header(review)
+        if params[:guitar_id]
+            content_tag(:h1, "Add a Review for #{review.guitar.kind} -  #{review.guitar.make.name}")
+        else
+          content_tag(:h1, "Create a review")
+        end
+      end
+    end
+
+
