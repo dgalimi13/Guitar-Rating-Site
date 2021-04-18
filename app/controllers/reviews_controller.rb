d27fc7b641518e683818901bd6ecba149eab084f
danny galimi <dannygalimi13@aim.com>
2021-04-18 14:33:11 -0400

    added redirects for not logged in reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
 
 def new
+    redirect_if_not_logged_in
     if @guitar = Guitar.find_by_id(params[:guitar_id])
         @review = @guitar.reviews.build
     else
